8d46c837ca2249eefeb40e6b9bcc9ab342a35dcf
itzprashanth <durgaprashanthgaddam@gmail.com>
2018-06-12 18:48:20 +0530

    added javadoc comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,6 +13,10 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
+  /**
+   * @javadoc
+  */
+
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
